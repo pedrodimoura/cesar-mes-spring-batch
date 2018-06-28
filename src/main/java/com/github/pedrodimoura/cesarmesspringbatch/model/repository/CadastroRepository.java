13bacb8f799499589ef927f660a5285911e6d4b7
Pedro Moura <pdimoura@gmail.com>
2018-06-28 16:56:38 -0300

    Deleting large CSV

--- src/main/java/com/github/pedrodimoura/cesarmesspringbatch/model/repository/CadastroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.pedrodimoura.cesarmesspringbatch.model.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.github.pedrodimoura.cesarmesspringbatch.model.entity.impl.Cadastro;
-
-public interface CadastroRepository extends CrudRepository<Cadastro, Long> {
-
-}
